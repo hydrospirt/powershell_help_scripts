2fe91a22a45884b330d28c18f2044c0c464c3c4b
Eduard Gumen <hydrospirt@mail.ru>
2024-06-01 18:24:40 +0300

    Fix RU lang, delete bios and cpu info

--- system_info.ps1
+++ system_info.ps1
@@ -1,22 +1,19 @@
-# РЈСЃС‚Р°РЅРѕРІРєР° РєРѕРґРёСЂРѕРІРєРё UTF-8
+# Установка кодировки UTF-8
 $OutputEncoding = [System.Text.Encoding]::UTF8
 $currentDateTime = Get-Date
 
 $computerSystem = Get-WmiObject -Class Win32_ComputerSystem
 $domain = $computerSystem.Domain
 $os = Get-CimInstance -ClassName Win32_OperatingSystem
-$bios = Get-WmiObject -Class Win32_BIOS
-$cpuinfo = Get-CimInstance -ClassName CIM_Processor
 
-$currentDateTime
-$env:USERNAME
+Write-Host "Текущая дата: $currentDateTime"
+Write-Host "Имя пользователя: $env:USERNAME"
+
+Write-Host "Имя компьютера: $env:COMPUTERNAME"
 if ($domain) {
-    Write-Host "РџРѕР»СЊР·РѕРІР°С‚РµР»СЊ РІРєР»СЋС‡РµРЅ РІ РґРѕРјРµРЅ: $domain"
+    Write-Host "Пользователь включен в домен: $domain"
 } else {
-    Write-Host "РџРѕР»СЊР·РѕРІР°С‚РµР»СЊ РЅРµ РІРєР»СЋС‡РµРЅ РІ РґРѕРјРµРЅ."
+    Write-Host "Пользователь не включен в домен."
 }
 $os | Format-List Caption, Version, BuildType, BuildNumber, InstallDate
-$cpuinfo | Format-List Name, Description, Version
-$bios
-
-Read-Host -Prompt "РќР°Р¶РјРёС‚Рµ Enter РґР»СЏ РІС‹С…РѕРґР°"
+Read-Host -Prompt "Нажмите Enter для выхода"
